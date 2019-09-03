4f805da858a8a5d890c0980f1349e7373048e6fe
jldeen <jedeen@outlook.com>
2019-09-03 16:44:12 -0700

    commit image processing work

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const Promise = require("bluebird");
 const request = require("request");
 const azure = require('azure-storage');
 const url = require('url');
+const sharp = require('sharp');
+const fs = require('fs');
+const format = require('./lib/format');
+const sizes = require('./lib/sizes');
 
 var options = {};
 
+//AzureStorageAdapter config
 class AzureStorageAdapter extends BaseStorage {
   constructor(config) {
     super();
@@ -28,53 +33,203 @@ class AzureStorageAdapter extends BaseStorage {
   }
 
   save(image) {
+    //create azure storage blob connection
     var fileService = azure.createBlobService(options.connectionString);
 
-    // Appends the dated folder if enabled
-    if (options.useDatedFolder) {
-      let date = new Date();
-      var uniqueName = "images" + "/" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + date.getHours() + date.getMinutes() + "_" + image.name;
-    }
-    else {
-      var uniqueName = "images" + "/" + image.name;
-    }
-
     return new Promise(function (resolve, reject) {
+      //create container if not exist
       fileService.createContainerIfNotExists(options.container, { publicAccessLevel: 'blob' }, function (error) {
         if (error)
           console.log(error);
         else {
-          console.log('Created the container or already existed. Container:' + options.container);
-            fileService.createBlockBlobFromLocalFile(options.container, uniqueName, image.path,     { 
-                  contentSettings: { 
-                    // TODO:add in contentType
-                    // contentType: 'image/png', 
-                    cacheControl: 'public, max-age=2592000' 
-                  } 
-                }, 
-            function (error) {
-            if (error) {
-              console.log(error);
-              reject(error.message);
-            }
-            else {
-              var urlValue = fileService.getUrl(options.container, uniqueName);
+          console.log('Created the container or already existed. Container: ' + options.container);
+            Promise.map(sizes, function(size) {
+              // remove extension, prep for webp format
+              var imageName = image.name.replace(/\.[^/.]+$/, "")
 
-              if (!options.cdnUrl) {
-                resolve(urlValue);
+               // Appends the dated folder if enabled
+              if (options.useDatedFolder) {
+                let date = new Date();
+
+                var blobName = "images" + "/" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + date.getHours() + date.getMinutes() + "_" + imageName;
               }
+              // dated folder set to false
+              else {
+                // var blobName = "images" + "/" + image.name;
+                var blobName = "images" + "/size/" + 'w'+ size.x + "/" + imageName + ".webp";
+              } 
+                // console.log(image)
 
-              var parsedUrl = url.parse(urlValue, true, true);
-              var protocol = (options.useHttps ? "https" : "http") + "://";
+                //raw image
+                var raw = image.path;
+                var blobNameRaw = "images" + "/" + "raw" + "/" + image.name;;
 
-              resolve(protocol + options.cdnUrl + parsedUrl.path);
-              // resolve(parsedUrl.path);
-            }
-          });
+                console.log("Your raw image path is: " + raw)
+
+                // full size .webp
+                var tmpFileFormat = "/tmp/output" + ".webp";
+                var blobNameResize = "images" + "/size/" + 'w'+ size.x + "/" + imageName + ".webp";
+
+                // 300, 600, 900, 1300w .webp
+                var tmpFileResize = "/tmp/output" + "-w" + size.x + ".webp"; 
+                var blobNameResize = "images" + "/" + "format" + "/" + image.name;
+                console.log("Your tmpFileResize is: " + tmpFileResize)
+
+                var uploadBlobs = [{
+                  name: raw
+                  },{
+                  name: tmpFileResize
+                  },{
+                  name: tmpFileFormat
+                }];
+
+                format(image.path, tmpFileFormat)
+
+                let transform = sharp( image.path )
+
+                return transform.resize( size.x ).webp().toBuffer().then( data => {
+                  // create files locally
+                  fs.writeFileSync(tmpFileResize, data);
+                  
+                   // upload image
+                   fileService.createBlockBlobFromLocalFile(options.container, blobName, tmpFileResize, { 
+                    contentSettings: { 
+                    contentType: image.type, 
+                    cacheControl: 'public, max-age=2592000' 
+                    } 
+                }, 
+                    function (error) {
+                    if (error) {
+                        console.log(error);
+                        reject(error.message);
+                    }
+                    else {
+                        // upload successful, get upload url
+                        var urlValue = fileService.getUrl(options.container, blobName);
+
+                        console.log('Uploaded blob name: ' + blobName + ',' + ' local image path is: ' + image.path);
+
+                        // no cdn, get azure storage url only
+                        if (!options.cdnUrl) {
+                        resolve(urlValue);
+
+                        console.log('CDN not specified, urlValue is: ' + urlValue);
+                        }
+
+                        //cdn url, get cdn url
+                        var parsedUrl = url.parse(urlValue, true, true);
+                        var protocol = (options.useHttps ? "https" : "http") + "://";
+                        var cdnUrl = protocol + options.cdnUrl + parsedUrl.path
+                        resolve(cdnUrl);
+
+                        console.log('CDN is specified, urlValue is: ' + cdnUrl);
+                        // resolve(parsedUrl.path);
+                    }
+                    });
+                })
+                  .catch( err => {
+                      console.log(err);
+                })
+              }
+          ); 
         }
-      });
     });
-  }
+  });
+}
+
+  // save(image) {
+  //   //create azure storage blob connection
+  //   var fileService = azure.createBlobService(options.connectionString);
+
+  //   return new Promise(function (resolve, reject) {
+  //     //create container if not exist
+  //     fileService.createContainerIfNotExists(options.container, { publicAccessLevel: 'blob' }, function (error) {
+  //       if (error)
+  //         console.log(error);
+  //       else {
+  //         console.log('Created the container or already existed. Container: ' + options.container);
+  //         var sizes = [{
+  //           path: 'large',
+  //               x: 900
+  //           },{
+  //           path: 'medium',
+  //               x: 600
+  //           },{
+  //           path: 'small',
+  //               x: 300
+  //       }];
+  //           Promise.map(sizes, function(size) {
+  //             // remove extension, prep for webp format
+  //             var imageName = image.name.replace(/\.[^/.]+$/, "")
+
+  //              // Appends the dated folder if enabled
+  //             if (options.useDatedFolder) {
+  //               let date = new Date();
+
+  //               var blobName = "images" + "/" + date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + date.getHours() + date.getMinutes() + "_" + imageName;
+  //             }
+  //             // dated folder set to false
+  //             else {
+  //               // var blobName = "images" + "/" + image.name;
+  //               var blobName = "images" + "/size/" + 'w'+size.x + "/" + imageName + ".webp";
+  //             }
+  //               var tmpFile = "/tmp/output" + "-w" + size.x + ".webp" 
+  //               console.log(image)
+
+  //               return sharp( image.path )
+  //                   .resize( size.x )
+  //                   .webp()
+  //                   .toBuffer()
+  //                   .then( data => {
+  //                     // create files locally
+  //                     fs.writeFileSync(tmpFile, data);
+
+  //                     // upload image
+  //                     fileService.createBlockBlobFromLocalFile(options.container, blobName, tmpFile,     { 
+  //                         contentSettings: { 
+  //                         contentType: image.type, 
+  //                         cacheControl: 'public, max-age=2592000' 
+  //                         } 
+  //                     }, 
+  //                         function (error) {
+  //                         if (error) {
+  //                             console.log(error);
+  //                             reject(error.message);
+  //                         }
+  //                         else {
+  //                             // upload successful, get upload url
+  //                             var urlValue = fileService.getUrl(options.container, blobName);
+      
+  //                             console.log('Uploaded blob name: ' + blobName + ',' + ' local image path is: ' + image.path);
+      
+  //                             // no cdn, get azure storage url only
+  //                             if (!options.cdnUrl) {
+  //                             resolve(urlValue);
+      
+  //                             console.log('CDN not specified, urlValue is: ' + urlValue);
+  //                             }
+      
+  //                             //cdn url, get cdn url
+  //                             var parsedUrl = url.parse(urlValue, true, true);
+  //                             var protocol = (options.useHttps ? "https" : "http") + "://";
+  //                             var cdnUrl = protocol + options.cdnUrl + parsedUrl.path
+  //                             resolve(cdnUrl);
+      
+  //                             console.log('CDN is specified, urlValue is: ' + cdnUrl);
+  //                             // resolve(parsedUrl.path);
+  //                         }
+  //                         });
+  //               })
+  //                   .catch( err => {
+  //                       console.log(err);
+  //                   })
+  //               }
+        
+  //           ); 
+  //       }
+  //     });
+  //   });
+  // }
 
   serve() {
     return function customServe(req, res, next) {
@@ -88,7 +243,6 @@ class AzureStorageAdapter extends BaseStorage {
 
   read(options) {
     return new Promise(function (resolve, reject) {
-
       var requestSettings = {
         method: 'GET',
         url: options.path,
@@ -104,7 +258,7 @@ class AzureStorageAdapter extends BaseStorage {
       });
     })
   }
-
 }
 
+module.exports = options;
 module.exports = AzureStorageAdapter;
